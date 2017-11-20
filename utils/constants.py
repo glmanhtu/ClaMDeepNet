0dcd94d8d2d15108271bd986f1bbf1ce8dfab20e
glmanhtu <glmanhtu@gmail.com>
2017-11-20 11:45:46 +0100

    rename default template to alexnet

--- utils/constants.py
+++ utils/constants.py
@@ -17,7 +17,7 @@ class Constant(object):
     # You also can change this value by set environment CAFFE_SOLVER
     CAFFE_SOLVER = "GPU"
 
-    GPU_ID = 0
+    GPU_ID = "0"
 
     WORKSPACE = "workspace"
 
