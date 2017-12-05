e6ae6de0b388db9ce455090f5c3f67fdf4c485cc
glmanhtu <glmanhtu@gmail.com>
2017-12-05 14:33:55 +0100

    ignore unnecessary package

--- deploy/roles/default.rb
+++
@@ -1,7 +0,0 @@
-name "default"
-description "Install Neuron Network Caffe"
-
-run_list(
-  "recipe[opencv]",
-  "recipe[neuron-network-caffe]"
-)
--- deploy/site-cookbooks/neuron-network-caffe/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'neuron-network-caffe'
-maintainer       'glmanhtu'
-maintainer_email 'glmanhtu@gmail.com'
-license          'All rights reserved'
-description      'Automatic install caffe for python'
-version          '0.1.0'
-depends 'poise-python'
