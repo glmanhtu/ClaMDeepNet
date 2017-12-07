b2ba056d803c67e71bcf2ed2ef19f88ac8dc1055
glmanhtu <glmanhtu@gmail.com>
2017-12-07 09:30:44 +0100

    update

--- data/random_photos.py
+++ data/random_photos.py
@@ -1,18 +1,16 @@
-"""Because we have alot of images, we're not be able to classify all of them (>100.000 images)
-So, we'll select random 1000 images, classify them & use them to train our neural network
-After that, we using trained network to classify the other images"""
-
 from get_all_photos import *
 from random import randint
 from shutil import copyfile
 import os
+import sys
 
 TOTAL_SAMPLE = 1000
-LABELED_IMAGES = os.path.join("images", "sample", "unknown")
+RANDOM_IMAGES = os.path.join("images", "sample", "unknown")
 
 
 if __name__ == '__main__':
-    path, dirs, files = os.walk(SAVE_IMAGE_PATH).next()
+    img_dir = sys.argv[1]
+    path, dirs, files = os.walk(img_dir).next()
     num_files = len(files)
     random_imgs = []
 
@@ -31,11 +29,11 @@ if __name__ == '__main__':
     current_index = 0
     copied = 0
 
-    for root, dirs, files in os.walk(SAVE_IMAGE_PATH):
+    for root, dirs, files in os.walk(img_dir):
         for f in files:
             image = os.path.join(root, f)
             if current_index in random_imgs:
-                copyfile(image, os.path.join(LABELED_IMAGES, f))
+                copyfile(image, os.path.join(RANDOM_IMAGES, f))
             print_progress(current_index, num_files, "Progress:", "Complete", 2, 50)
             current_index += 1
 
