c8607fb22c7db0bbcc35df2465495780ac522592
glmanhtu <glmanhtu@gmail.com>
2017-12-28 11:33:12 +0100

    Update

---
+++ utils/hidden_print.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+
+class HiddenPrints:
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout = self._original_stdout
