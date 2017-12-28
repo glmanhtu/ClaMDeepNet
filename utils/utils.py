934a315c86f65988d1b871d0f399dd38c983359d
glmanhtu <glmanhtu@gmail.com>
2017-12-28 20:38:30 +0100

    Update

--- utils/utils.py
+++ utils/utils.py
@@ -4,11 +4,7 @@ import shutil
 import subprocess
 import sys
 from subprocess import call
-import matplotlib
-matplotlib.use('Agg')
 import cv2
-import matplotlib.pylab as plt
-import pandas as pd
 from mako.template import Template
 
 import pycaffe
@@ -90,56 +86,3 @@ def empty_dir(dir):
     if os.path.isdir(dir):
         shutil.rmtree(dir)
     os.makedirs(dir)
-
-
-def draw_curve(caffe_log, image_path, template, trained_model):
-    cwd = os.getcwd()
-    os.chdir(os.path.dirname(caffe_log))
-    '''
-        Generating training and test logs
-        '''
-    # Parsing training/validation logs
-    command = caffe.caffe_home() + '/tools/extra/parse_log.sh ' + caffe_log
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-    process.wait()
-    # Read training and test logs
-    train_log_path = caffe_log + '.train'
-    test_log_path = caffe_log + '.test'
-    train_log = pd.read_csv(train_log_path, delim_whitespace=True)
-    test_log = pd.read_csv(test_log_path, delim_whitespace=True)
-
-    '''
-    Making learning curve
-    '''
-    plt.figure(figsize=(12, 6), dpi=120)
-    fig, ax1 = plt.subplots()
-
-    # Plotting training and test losses
-    train_loss, = ax1.plot(train_log['#Iters'], train_log['TrainingLoss'], color='red', alpha=.5)
-    test_loss, = ax1.plot(test_log['#Iters'], test_log['TestLoss'], linewidth=1, color='green')
-    ax1.set_ylim(ymin=0, ymax=1)
-    ax1.set_xlabel('Iterations', fontsize=14)
-    ax1.set_ylabel('Loss', fontsize=14)
-    ax1.tick_params(labelsize=11)
-    # Plotting test accuracy
-    ax2 = ax1.twinx()
-    test_accuracy, = ax2.plot(test_log['#Iters'], test_log['TestAccuracy'], linewidth=1, color='blue')
-    ax2.set_ylim(ymin=0, ymax=1)
-    ax2.set_ylabel('Accuracy', fontsize=14)
-    ax2.tick_params(labelsize=11)
-    # Adding legend
-    plt.legend([train_loss, test_loss, test_accuracy], ['Training Loss', 'Test Loss', 'Test Accuracy'],
-               bbox_to_anchor=(1, 0.9))
-    title = 'Net Model ' + template
-    if trained_model != "":
-        title += " - Finetune"
-    plt.title(title, fontsize=16)
-    # Saving learning curve
-    plt.savefig(image_path)
-
-    '''
-    Deleting training and test logs
-    '''
-    os.remove(train_log_path)
-    os.remove(test_log_path)
-    os.chdir(cwd)
