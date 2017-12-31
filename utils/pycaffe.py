f31b7ff7e6899cbc8be3c2c978810763194d3018
glmanhtu <glmanhtu@gmail.com>
2017-12-31 10:08:07 +0100

    update

--- utils/pycaffe.py
+++ utils/pycaffe.py
@@ -28,23 +28,27 @@ class PyCaffe(object):
         command = ' '.join(command)
         utils.execute_command(test_id, command)
 
-    def get_resume_sloverstate(self, ws, snapshot_prefix):
-        snapshot_path = os.path.dirname(snapshot_prefix)
+    def get_resume_sloverstate(self, test_id, ws, snapshot_prefix):
+        utils.put_message(("log", test_id, "checking for resume"))
+        snapshot_path = ws.workspace("caffe_model")
         max_iter = 0
+        sloverstate = ""
         for file in listdir(snapshot_path):
             file_path = join(snapshot_path, file)
             if isfile(file_path):
-                if "sloverstate" in file_path:
-                    curr_iter = int(re.findall('snapshot_iter_(\d+)\.solverstate', file_path)[0])
+                if "sloverstate" in file:
+                    curr_iter = int(re.findall('snapshot_iter_(\d+)\.solverstate', file)[0])
                     if max_iter < curr_iter:
                         max_iter = curr_iter
-        return max_iter
+                        sloverstate = file_path
+        utils.put_message(("log", test_id, "current iter: %d" % max_iter))
+        return sloverstate
 
     def train(self, solver, log, gpu_id, trained_model, ws, test_id, total_iter, snapshot_prefix):
         solver = os.path.abspath(solver)
         log = os.path.abspath(log)
         caffe_bin = caffe_home() + "/build/tools/caffe"
-        resume_sloverstate = self.get_resume_sloverstate(ws, snapshot_prefix)
+        resume_sloverstate = self.get_resume_sloverstate(test_id, ws, snapshot_prefix)
         command = ["GLOG_minloglevel=0", caffe_bin, "train", "--solver=" + solver]
 
         if trained_model != "":
@@ -52,9 +56,10 @@ class PyCaffe(object):
             if not file_already_exists(trained_model_path):
                 download_file_strategy(trained_model, trained_model_path)
             command.extend(["--weights", trained_model_path])
-        if resume_sloverstate != 0:
-            if resume_sloverstate == total_iter:
+        if resume_sloverstate != "":
+            if str(total_iter) + ".sloverstate" in resume_sloverstate:
                 return
+            command.extend(["--snapshot", resume_sloverstate])
         if Constant.CAFFE_SOLVER == "GPU":
             command.extend(["-gpu=" + gpu_id])
 
