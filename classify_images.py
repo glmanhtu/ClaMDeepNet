efa700ca74f0c823b67333d06a157a3c1428914d
glmanhtu <glmanhtu@gmail.com>
2018-01-09 09:28:15 +0100

    update

--- classify_images.py
+++ classify_images.py
@@ -64,6 +64,9 @@ def copytree(src, dst, symlinks=False, ignore=None):
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
+        if os.path.exists(d):
+            shutil.rmtree(d)
+            os.makedirs(d)
         if os.path.isdir(s):
             shutil.copytree(s, d, symlinks, ignore)
         else:
