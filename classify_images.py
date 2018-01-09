eec779cda4d4e3af6ad81f95f9a4297f2fb2209d
glmanhtu <glmanhtu@gmail.com>
2018-01-09 09:32:11 +0100

    update

--- classify_images.py
+++ classify_images.py
@@ -61,13 +61,12 @@ def generate_workspace(test_id):
 
 
 def copytree(src, dst, symlinks=False, ignore=None):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
         if os.path.isdir(s):
-            if os.path.exists(d):
-                shutil.rmtree(d)
-                os.makedirs(d)
             shutil.copytree(s, d, symlinks, ignore)
         else:
             shutil.copy2(s, d)
